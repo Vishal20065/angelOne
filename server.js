51a8ebc995d292475074bb4b32d8405ee3835ded
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-11-03 12:52:41 +0530

    qr added

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ import profileRoutes from "./routes/profileRoutes.js";
 import order1Routes from "./routes/order1Routes.js";
 import order2Routes from "./routes/order2Routes.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// make sure public folder exists
+
+
 import QRCode from "qrcode";
 import cors from 'cors'
 
@@ -16,7 +26,11 @@ import cors from 'cors'
 dotenv.config();
 const app = express();
 
+const publicDir = path.join(__dirname, "public");
+if (!fs.existsSync(publicDir)) fs.mkdirSync(publicDir);
+
 app.use(express.json());
+app.use(express.static(publicDir));
 
 app.use(
   cors({
@@ -37,6 +51,37 @@ app.use("/api/orders2", order2Routes);
 
 
 
+
+
+app.get("/generateQR", async (req, res) => {
+  try {
+const data = `
+Name: John Doe
+Email: john@example.com
+Post: Marketing Manager
+Profile Picture: https://yourdomain.com/images/john.jpg
+    `;
+
+    const filePath = path.join(publicDir, "profileQR.png");
+
+    // generate QR code image
+    await QRCode.toFile(filePath, data);
+
+    // return public link
+    const imageUrl = `https://angelone-7kss.onrender.com/profileQR.png`;
+    res.json({ success: true, url: imageUrl });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Failed to generate QR" });
+  }
+});
+
+app.get("/",(req,res)=>{
+  res.send("welcome to server")
+})
+
+
+
 const data = `
 Name: John Doe
 Email: john@example.com
@@ -48,6 +93,8 @@ QRCode.toFile("profileQR.png", data, (err) => {
   if (err) throw err;
   console.log("QR Code created!");
 });
+
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
